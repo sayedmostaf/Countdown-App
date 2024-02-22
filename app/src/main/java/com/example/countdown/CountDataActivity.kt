9bd39acc39e945e7a692f7a8f492f08311812aa6
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-02-22 14:41:34 +0200

    Initial commit

--- app/src/main/java/com/example/countdown/CountDataActivity.kt
+++ app/src/main/java/com/example/countdown/CountDataActivity.kt
@@ -1,27 +1,97 @@
 package com.example.countdown
 
+import android.annotation.SuppressLint
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.cardview.widget.CardView
+import java.time.Duration
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class CountDataActivity : AppCompatActivity() {
-    lateinit var namePreview: TextView
-    lateinit var imageView: ImageView
+    // Initializing UI elements
+    private lateinit var nameView: TextView
+    private lateinit var imageView: ImageView
+    private lateinit var ageView: TextView
+    private lateinit var timeLeftView: TextView
+    private lateinit var daysView: TextView
+    private lateinit var hoursView: TextView
+    private lateinit var minutesView: TextView
+    private lateinit var secondsView: TextView
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_count_data)
-        val intent = getIntent()
+
+        // Retrieving data from intent
+        val intent = intent
         val receivedName = intent.getStringExtra("name")
         val imageUriString = intent.getStringExtra("imageUri")
-        namePreview = findViewById(R.id.name_text)
+        val receivedAge = intent.getIntExtra("age", 0)
+        val birthday = intent.getStringExtra("birthday")
+
+        // Initializing UI elements by finding views by ID
+        nameView = findViewById(R.id.name_text)
         imageView = findViewById(R.id.profile_image)
-        namePreview.text = receivedName
-        if (imageUriString != null) {
-            val imageUri = Uri.parse(imageUriString)
+        ageView = findViewById(R.id.age_text)
+        timeLeftView = findViewById(R.id.time_left_text)
+        daysView = findViewById(R.id.days_text)
+        hoursView = findViewById(R.id.hours_text)
+        minutesView = findViewById(R.id.minutes_text)
+        secondsView = findViewById(R.id.seconds_text)
+
+        // Setting received data to UI elements
+        nameView.text = receivedName
+        ageView.text = receivedAge.toString()
+        val newAge = receivedAge + 1
+        timeLeftView.text = "Time Left To $newAge's Birthday"
+
+        // Setting profile image if available
+        val imageUri = imageUriString?.let { Uri.parse(it) }
+        if (imageUri != null) {
             imageView.setImageURI(imageUri)
         }
+
+        // Calculate time until next birthday and starting countdown timer
+        var next = LocalDate.parse(
+            birthday,
+            DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault())
+        )
+        next = next.plusYears(receivedAge.toLong())
+        if (LocalDate.now() > next) next = next.plusYears(1)
+        val millisUntilNextBirthday =
+            Duration.between(LocalDateTime.now(), next.atStartOfDay()).toMillis()
+        object : CountDownTimer(millisUntilNextBirthday, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                var temp = millisUntilFinished
+                // Updating countdown values on each tick
+                // The remaining days
+                daysView.text = (temp / (24 * 60 * 60 * 1000)).toString()
+                if (daysView.text.length < 2) daysView.text = "0${daysView.text}"
+                temp %= (24 * 60 * 60 * 1000)
+                // The remaining hours
+                hoursView.text = (temp / (60 * 60 * 1000)).toString()
+                if (hoursView.text.length < 2) hoursView.text = "0${hoursView.text}"
+                temp %= (60 * 60 * 1000)
+                // The remaining minutes
+                minutesView.text = (temp / (60 * 1000)).toString()
+                if (minutesView.text.length < 2) minutesView.text = "0${minutesView.text}"
+                temp %= (60 * 1000)
+                // The remaining seconds
+                secondsView.text = (temp / 1000).toString()
+                if (secondsView.text.length < 2) secondsView.text = "0${secondsView.text}"
+            }
+
+            override fun onFinish() {
+                // Displaying birthday message when countdown finishes
+                timeLeftView.text = "Happy Birthday"
+            }
+        }.start() // Starting the countdown timer
     }
 }
--- app/src/main/java/com/example/countdown/InputDateActivity.kt
+++ app/src/main/java/com/example/countdown/InputDateActivity.kt
@@ -15,7 +15,10 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import java.lang.Exception
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.Calendar
+import java.util.Locale
 
 class InputDateActivity : ComponentActivity() {
     // Declare variables
@@ -24,8 +27,8 @@ class InputDateActivity : ComponentActivity() {
     private lateinit var nameField: TextView
     private var selectedImageUri: Uri? = null
 
-    // make the photo clickable and pick photo from gallery
-    var pickImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+    // Activity result launcher for picking an image
+    private var pickImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
         if (uri != null) {
             selectedImageUri = uri
             image.setImageURI(uri)
@@ -35,14 +38,14 @@ class InputDateActivity : ComponentActivity() {
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // image action
         setContentView(R.layout.activity_input_data)
+
+        // Find and set click listener for the profile image
         image = findViewById(R.id.profile_image)
         image.setOnClickListener { pickImage.launch("image/*") }
-
-        // take a date from dialog
+        // Find and set click listener for the birthday field to show date picker
         birthdayField = findViewById(R.id.birthday_field)
-        birthdayField.setOnTouchListener { v, event ->
+        birthdayField.setOnTouchListener { _, event ->
             if (event.action == MotionEvent.ACTION_UP) {
                 // Calculate the position of the touch
                 val drawableEnd = birthdayField.compoundDrawablesRelative[2]
@@ -55,22 +58,26 @@ class InputDateActivity : ComponentActivity() {
             false
         }
 
-        // confirm button actions
+        // Find and set click listener for the confirm button
         val calculateButton: Button = findViewById(R.id.confirm_button)
         nameField = findViewById(R.id.name_field)
         calculateButton.setOnClickListener {
-
             try {
                 if (nameField.text.toString().trim().isEmpty()) throw Exception("Enter Your name")
                 else if (birthdayField.text.toString().trim()
                         .isEmpty()
                 ) throw Exception("Enter Your birthday")
                 else {
-                    // send data from fields
+                    val birthday = birthdayField.text.toString()
+                    val age = calculateAge(birthday)
+
+                    // Send data to CountDateActivity
                     val name = nameField.text.toString()
                     val intent = Intent(this, CountDataActivity::class.java)
                     intent.putExtra("imageUri", selectedImageUri.toString())
                     intent.putExtra("name", name)
+                    intent.putExtra("age", age)
+                    intent.putExtra("birthday", birthday)
                     startActivity(intent)
                 }
             } catch (ex: Exception) {
@@ -79,22 +86,36 @@ class InputDateActivity : ComponentActivity() {
         }
     }
 
+    // Calculate age based on birthday date
+    private fun calculateAge(birthDate: String): Int {
+        val birthday = LocalDate.parse(
+            birthDate,
+            DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault())
+        )
+        val today = LocalDate.now()
+        var age = today.year - birthday.year
+        if (today.monthValue < birthday.monthValue ||
+            (today.monthValue == birthday.monthValue && today.dayOfMonth < birthday.dayOfMonth)
+        ) age--
+
+        return age
+    }
+
+    // Show date picker dialog
     private fun showDatePickerDialog() {
         val calendar = Calendar.getInstance()
         val datePickerDialog = DatePickerDialog(
             this,
             { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
                 // Handle the selected date
-                val selectedDate = "$year-${month + 1}-$dayOfMonth"
+                val selectedDate = String.format("%03d-%02d-%02d", year, month + 1, dayOfMonth)
                 birthdayField.setText(selectedDate)
             },
             calendar.get(Calendar.YEAR),
             calendar.get(Calendar.MONTH),
             calendar.get(Calendar.DAY_OF_MONTH)
         )
-        // Set the maximum date to the current date
         datePickerDialog.datePicker.maxDate = calendar.timeInMillis
         datePickerDialog.show()
     }
-
 }
